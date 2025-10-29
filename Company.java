b9f0e6a7cf399c1fdc7ceb0e85ad396a3850c244
ASHWIN G <311823205008@msajce-edu.in>
2025-10-29 21:54:52 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,7 @@
+class Company {
+    public static void main(String[] args) {
+        String a = "ABC";
+        String b = "Mumbai";
+        System.out.println(a + " Technologies " + b);
+    }
+}
