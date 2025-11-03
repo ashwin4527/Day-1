f6d96bc1e1c52fbe6e0286b69b7624781db81fde
ASHWIN G <311823205008@msajce-edu.in>
2025-11-03 14:31:40 +0530

    Add files via upload

---
+++ AlphabeticalOrder.java
@@ -0,0 +1,15 @@
+public class AlphabeticalOrder {
+    public static void main(String[] args) {
+        // Initialize two character variables
+        char ch1 = 's';
+        char ch2 = 'e';
+
+        // Compare and display in alphabetical order
+        if (ch1 < ch2) {
+            System.out.println(ch1 + ", " + ch2);
+        } else {
+            System.out.println(ch2 + ", " + ch1);
+        }
+    }
+}
+
